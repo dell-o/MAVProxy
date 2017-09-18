606e05a47ad00ca0e89c1639848b283c4cb49d8a
dell-o <jakedell95@gmail.com>
2017-09-19 01:21:03 +1000

    Functional aircraftLink module with EFLS

--- MAVProxy/modules/mavproxy_efls/__init__.py
+++ MAVProxy/modules/mavproxy_efls/__init__.py
@@ -13,7 +13,7 @@ import os
 import os.path
 import sys
 import math
-from pymavlink import mavutil
+from pymavlink import mavutil, mavwp
 import errno
 import time
 from MAVProxy.modules.mavproxy_example import mavProxyLink_pb2
@@ -30,19 +30,25 @@ class efls(mp_module.MPModule):
         
         super(efls, self).__init__(mpstate, "efls", "")
         self.status_callcount = 0
-        self.boredom_interval = 10 # seconds
-        self.last_bored = time.time()
-
+        self.check_interval = 1 # seconds
+        self.last_check = time.time()
+        
+        self.wploader = mavwp.MAVWPLoader()
+        self.num_wps_expected = 0
+        self.wps_received = {}
+        self.expect_waypoints = 0
+        self.timeout = 0
+        
+        self.seq = 1
+        self.frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+        
         self.packets_mytarget = 0
         self.packets_othertarget = 0
         self.verbose = False
         
-        self.time_previous = 0
-        self.protocol_interval = 1
-        
         # ToDo - Change paths
-        self.protocolbuf_in = "/home/dello/workspace/EFL/aircraftLink_medium_waypoints"
-        self.protocolbuf_out = "/home/dello/workspace/EFL/aircraftLink_medium_aircraft"
+        self.protocolbuf_in = "/home/dello/workspace/EFLS/build/res/protobuf/aircraftLink_medium_waypoints"
+        self.protocolbuf_out = "/home/dello/workspace/EFLS/build/res/protobuf/aircraftLink_medium_aircraft"
 
         self.efls_settings = mp_settings.MPSettings([ ('verbose', bool, False), ])
         self.add_command('efls', self.cmd_efls, "efls module", ['status','set (LOGSETTING)'])
@@ -65,42 +71,134 @@ class efls(mp_module.MPModule):
     def status(self):
         '''returns information about module'''
         self.status_callcount += 1
-        self.last_bored = time.time() # status entertains us
+        self.last_check = time.time() # status entertains us
         return("status called %(status_callcount)d times.  My target positions=%(packets_mytarget)u  Other target positions=%(packets_mytarget)u" %
                {"status_callcount": self.status_callcount,
                 "packets_mytarget": self.packets_mytarget,
                 "packets_othertarget": self.packets_othertarget,
                })
 
-    def boredom_message(self):
-        if self.efls_settings.verbose:
-            return ("I'm very bored")
-        return ("I'm bored")
-
     def idle_task(self):
         '''called rapidly by mavproxy'''
         now = time.time()
-        if now-self.last_bored > self.boredom_interval:
-            self.last_bored = now
-            message = self.boredom_message()
-            self.say("%s: %s" % (self.name,message))
-            # See if whatever we're connected to would like to play:
-            self.master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_NOTICE, message)
-            message = "A test of sanity"
-            self.say("%s: %s" % (self.name,message))
-            self.master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_NOTICE, message)                         
-                                            
-            w = mavutil.mavlink.MAVLink_mission_item_message(0, 0, 1, 10, 16,
-                                                          0, 1, 0, 0, 0, 0, -35.372532, 149.163345, 120.000000)
-
-            self.module('wp').wploader.add(w)
-            self.master.mav.send(self.module('wp').wploader.wp(w.seq))
-
-            #tell the wp module to expect some waypoints
-            self.module('wp').loading_waypoints = True
+        if now-self.last_check > self.check_interval:
+            self.last_check = now
+                   
+            self.aircraftFunc()
+            self.waypointFunc()
+            
+            
+    def aircraftFunc(self):
+        '''writes aircraft data to Protobuf file'''
+        
+        # Defines aircarft data in Protobuf
+        aircraft_link_out = mavProxyLink_pb2.AircraftLink()
+            
+        aircraft = aircraft_link_out.aircrafts.add()
+        aircraft.lat = self.lat
+        aircraft.lon = self.lon
+        aircraft.bearing = self.bearing
+        aircraft.speed = self.speed
+        aircraft.altitude = self.altitude
+        aircraft.wind_speed = self.wind_speed
+        aircraft.wind_direction = self.wind_direction
+        aircraft.motor_current = self.motor_current
+        aircraft.motor_throttle = self.motor_throttle
+        
+        # Writes aircraft data to Protobuf file
+        f = open(self.protocolbuf_out, "wb")
+        f.write(aircraft_link_out.SerializeToString())
+        f.flush()
+        f.close
+                    
+            
+    def waypointFunc(self):
+        '''reads waypoints from Protobuf file'''
+        
+        # Read waypoints from protobuf
+        aircraft_link_in = mavProxyLink_pb2.AircraftLink()
+        f = open(self.protocolbuf_in, "rb")
+        aircraft_link_in.ParseFromString(f.read())
+        f.close()
+        #self.master.waypoint_request_list_send()
+        
+        # Detects new waypoints
+        if len(aircraft_link_in.waypoints) > 0:
+            
+            # Request updated waypoints
+            if self.expect_waypoints == 0 or self.timeout > 3:
+                self.timeout = 0
+                self.wps_received = {}
+                self.wploader.clear()
+                self.expect_waypoints = 1
+                self.mpstate.module('wp').cmd_wp(['list'])
+                print 'EFLS: Getting waypoints'
+                return
+            if (len(self.wps_received) != self.num_wps_expected):
+                self.timeout += 1
+                print 'EFLS: Fetch not completed'
+                return    
+            print 'EFLS: Waypoints fetched'  
+            self.expect_waypoints = 0    
+            self.timeout = 0
+            
+            # Find EFLS do_land_start --> This is the last do_land_start
+            wp_pos = -1
+            for i in range(self.wploader.count()):
+                w_current = self.wploader.wp(i)
+                if w_current.command == mavutil.mavlink.MAV_CMD_DO_LAND_START:
+                    wp_pos = w_current.seq + 1
+            if wp_pos == -1:
+                print 'EFLS: Could not find (Do Land Start) mission item'
+                return
+            
+            # Removes old waypoints --> Required to ensure if less waypoints are added, old ones do not survive
+            for i in range(wp_pos,self.wploader.count()):
+                 self.wploader.remove(self.wploader.wp(wp_pos))
+           
+            # Read waypoints 
+            num = wp_pos
+            for Waypoints in aircraft_link_in.waypoints:
+                for Waypoint in Waypoints.waypoint:
+                    lat = Waypoint.lat
+                    lon = Waypoint.lon
+                    alt = Waypoint.altitude
+                    # Set waypoints
+                    m = mavutil.mavlink.MAVLink_mission_item_message(self.target_system, self.target_component, num, self.frame, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, lat, lon, alt)
+                    self.mpstate.module('wp').wploader.set(m, m.seq)
+                    self.master.mav.send(self.mpstate.module('wp').wploader.wp(m.seq))
+                    self.mpstate.module('wp').loading_waypoints = True
+                    num += 1
+            
+            # Send new mission to Aircraft        
+            self.mpstate.module('wp').send_all_waypoints()
+            
+            # Clear waypoint data from Protobuf file
+            f = open(self.protocolbuf_in, "wb")
+            f.close()
+            
+            # Trigger EFLS landing
+            self.master.waypoint_set_current_send(wp_pos)
+            
+            print 'EFLS: Success'            
+                
 
     def mavlink_packet(self, m):
         '''handle mavlink packets'''
+        
+        # Waypoint handling
+        if self.expect_waypoints == 1:
+            if m.get_type() in ['WAYPOINT_COUNT','MISSION_COUNT']:
+                self.num_wps_expected = m.count
+            if m.get_type() in ['WAYPOINT', 'MISSION_ITEM']:
+                if (len(self.wps_received) < self.num_wps_expected):
+                    if (m.seq not in self.wps_received.keys()):
+                        self.wps_received[m.seq] = True
+                        self.wploader.set(m, m.seq)
+                    else:
+                        self.expect_waypoints = 2
+        
+        # Aircraft message handling    
         if m.get_type() == 'GLOBAL_POSITION_INT':
             self.lat = m.lat * 1e-7
             self.lon = m.lon * 1e-7
@@ -115,48 +213,7 @@ class efls(mp_module.MPModule):
             self.wind_speed = m.speed
             self.wind_direction = m.direction
             
-            
-        now = time.time()
-        if now - self.time_previous > self.protocol_interval:
-            aircraft_link_out = mavProxyLink_pb2.AircraftLink()
-            self.time_previous = now
-            #try:
-            #    f = open("cat.txt", "rb")
-            #    aircraft_link.ParseFromString(f.read())
-            #    f.close()
-            #except IOError:
-            #    print "Could not connect to Protocol Buf file. Creating new one."
-            
-            aircraft = aircraft_link_out.aircrafts.add()
-            aircraft.lat = self.lat
-            aircraft.lon = self.lon
-            aircraft.bearing = self.bearing
-            aircraft.speed = self.speed
-            aircraft.altitude = self.altitude
-            aircraft.wind_speed = self.wind_speed
-            aircraft.wind_direction = self.wind_direction
-            aircraft.motor_current = self.motor_current
-            aircraft.motor_throttle = self.motor_throttle
-            
-            f = open(self.protocolbuf_out, "wb")
-            f.write(aircraft_link_out.SerializeToString())
-            f.flush()
-            f.close
-            
-            del aircraft
-            
-            # Read waypoints
-            #aircraft_link_in = mavProxyLink_pb2.AircraftLink()
-            #f = open(protocolbuf_in, "rb")
-            #aircraft_link_in.ParseFromString(f.read())
-            #f.close()
-            
-            #for aircraft in aircraft_link_in.aircrafts:
-            #    message = str(aircraft.speed)
-            #self.say("%s: %s" % (self.name,message))
-            ## See if whatever we're connected to would like to play:
-            #self.master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_NOTICE, message)
 
 def init(mpstate):
     '''initialise module'''
-    return example(mpstate)
+    return efls(mpstate)
